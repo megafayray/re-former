35ced9a26a9db106be20fd3f3f78e658132ddb9c
megafayray <fay.brotherston@gmail.com>
2025-07-12 22:29:11 -0400

    Add editing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,9 +15,24 @@ class UsersController < ApplicationController
         end
     end
 
+    def edit
+        @user = User.find(params[:id])
+    end
+    
+    def update
+        @user = User.find(params[:id])
+
+        if @user.update(user_params)
+            redirect_to edit_user_path(@user), notice: "User updated successfully."
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
     private
 
     def user_params
-        params.expect(user: [:username, :email, :password])
+        # params.expect(user: [:username, :email, :password])
+        params.require(:user).permit(:username, :email, :password)
     end
 end
